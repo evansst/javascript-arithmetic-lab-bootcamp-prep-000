41985b4bc8a0ec674152f7f8f65f8017dad87b3d
thoughtful-bundle-9618 <evansst@icloud.com>
2020-06-08 04:33:38 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,35 @@
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a,b) {
+  return a - b; 
+}
+
+function multiply(a,b) {
+  return a * b;
+}
+ 
+function divide(a,b) {
+  return a/b;
+}
+
+function increment(n) {
+  n++
+  return n;
+}
+
+function decrement(n) {
+  n--
+  return n;
+} 
+
+function makeInt(a) {
+  parseInt (a, 10)
+  return a;
+}
+
+function preserveDecimal(n) {
+  parseFloat(n)
+  return n;
+}
